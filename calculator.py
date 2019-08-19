1ae5b5aa49a85e51fce2f9fe0f42cea9842206a9
Mostafa Ramezani <crystalsoft2010@gmail.com>
2019-08-19 17:27:24 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,1 @@
-"""
-Calculator library containing basic math operations.
-"""
-def add(first_term, second_term):
-  return first_term+second_term
-def subtract(first_term, second_term):
-  return first_term-second_term
+
