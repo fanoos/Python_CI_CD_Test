51d37d9320ff3ac250e7fefe35357fbdd1d09d39
Mostafa Ramezani <crystalsoft2010@gmail.com>
2019-09-26 16:19:38 +0200

    pypi pipeline

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
+import os
 
+if os.environ.get('CI_COMMIT_TAG'):
+    version = os.environ['CI_COMMIT_TAG']
+else:
+    version = os.environ['CI_JOB_ID']
 with open("README.md","r") as fh:
     long_description= fh.read()
 
 setup(
     name='Python_DevOps',
-    version='0.0.6',
+    #version='0.0.6',
+    version=version,
     author='Mostafa Ramezani',
     author_email='crystalsoft2010@gmail.com',
     description='Sample of How Implement CI for python project and Build Docker image for Shippable product.',
