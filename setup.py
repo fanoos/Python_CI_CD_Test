52fb389afc094252e1a47dc02f6a4789287b265a
Mostafa Ramezani <crystalsoft2010@gmail.com>
2019-09-27 15:41:30 +0200

    up setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Testing',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
